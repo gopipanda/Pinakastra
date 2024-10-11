0a13cf5a9f3fb76c3a87d0d6e59e71ed4c65b00a
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-11 12:14:00 +0530

    Update script8.sh

--- script8.sh
+++ script8.sh
@@ -1,8 +1,2 @@
 set -x
-set -e
-
-source /home/pinaka/all_in_one/vpinakastra/bin/activate
-export ANSIBLE_PYTHON_INTERPRETER=/root/vpinakastra/bin/python
-kolla-ansible post-deploy
-pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/2023.1
-/home/pinaka/all_in_one/vpinakastra/share/kolla-ansible/init-runonce
+echo "$HOSNAME"
