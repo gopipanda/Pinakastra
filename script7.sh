6acaffa183e8f340b59b54eb356e55253a8406e0
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-11 12:13:47 +0530

    Update script7.sh

--- script7.sh
+++ script7.sh
@@ -1,11 +1,2 @@
 set -x
-set -e
-
-source /home/pinaka/all_in_one/vpinakastra/bin/activate
-export ANSIBLE_PYTHON_INTERPRETER=/home/pinaka/all_in_one/vpinakastra/bin/python
-echo "export ANSIBLE_PYTHON_INTERPRETER=/root/vpinakastra/bin/python" >> /etc/bashrc
-cd /home/pinaka/all_in_one/vpinakastra/
-kolla-ansible -i ./all-in-one bootstrap-servers
-sudo sed -i.bak 's|^127\.0\.0\.1[[:space:]]*localhost[[:space:]]*|#127.0.0.1   localhost|' /etc/hosts
-kolla-ansible -i ./all-in-one prechecks
-kolla-ansible -i ./all-in-one deploy
+echo "$HOSNAME"
