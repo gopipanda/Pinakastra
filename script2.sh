681a5dd8e89c6bd5e883d4678062d54b85ab1578
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-14 12:25:15 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,2 +1,12 @@
 set -x
-echo "$HOSNAME"
+#!/bin/bash
+
+# Accessing environment variables
+echo "Hostname: $HOSTNAME"
+echo "IP Address: $IP_ADDRESS"
+echo "Netmask: $NETMASK"
+echo "Interface 1: $INTERFACE_01"
+echo "Interface 2: $INTERFACE_02"
+echo "Gateway: $GATEWAY"
+echo "DNS Servers: $DNS_SERVERS"
+echo "Root User Password: $ROOT_USER_PASSWORD"
