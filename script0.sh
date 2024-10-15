eb474631768d1c78ba01713b1e1f1a14c5440d37
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-15 18:27:46 +0530

    Update script0.sh

--- script0.sh
+++ script0.sh
@@ -3,12 +3,15 @@ set -ex  # Fail the script if any command fails
 echo "$IP_ADDRESS"
 echo "Creating volumes pool..."
 sudo cephadm shell -- ceph osd pool create volumes
-
+sleep 50
 echo "Creating images pool..."
 sudo cephadm shell -- ceph osd pool create images
+sleep 50
 
 echo "Creating backups pool..."
 sudo cephadm shell -- ceph osd pool create backups
+sleep 50
 
 echo "Creating vms pool..."
 sudo cephadm shell -- ceph osd pool create vms
+sleep 50
