5560d06d994f6f87f83acc2e373d320cb3a5f9af
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-15 18:46:07 +0530

    Update script0.sh

--- script0.sh
+++ script0.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ex  # Debugging and exit on error
 
-sudo cephadm shell -- bash -c "
-  ceph osd pool create volumes;
-  sleep 2;
-  ceph osd pool create images;
-  sleep 2;
-  ceph osd pool create backups;
-  sleep 2;
-  ceph osd pool create vms;
-"
+#!/bin/bash
+set -e
+
+sudo cephadm shell -- ceph osd pool create volumes
+sleep 2
+
+sudo cephadm shell -- ceph osd pool create images
+sleep 2
+
+sudo cephadm shell -- ceph osd pool create backups
+sleep 2
+
+sudo cephadm shell -- ceph osd pool create vms
+
