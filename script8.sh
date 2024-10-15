c23092690556e2dd69fc65349890aa83d3cd2b78
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-15 15:45:29 +0530

    Update script8.sh

--- script8.sh
+++ script8.sh
@@ -2,11 +2,8 @@ set -x
 #!/bin/bash
 
 # Accessing environment variables
-echo "Hostname: $HOSTNAME"
-echo "IP Address: $IP_ADDRESS"
-echo "Netmask: $NETMASK"
-echo "Interface 1: $INTERFACE_01"
-echo "Interface 2: $INTERFACE_02"
-echo "Gateway: $GATEWAY"
-echo "DNS Servers: $DNS_SERVERS"
-echo "Root User Password: $ROOT_USER_PASSWORD"
+source /home/pinaka/all_in_one/vpinakastra/bin/activate
+export ANSIBLE_PYTHON_INTERPRETER=/root/vpinakastra/bin/python
+kolla-ansible post-deploy
+pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/2023.1
+/home/pinaka/all_in_one/vpinakastra/share/kolla-ansible/init-runonce
