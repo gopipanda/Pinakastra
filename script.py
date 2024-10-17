9fa831fd3d5cbe1c748f71a71255c5aad7d3db58
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-17 17:21:12 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+def load_config(json_file):
+    """Read the JSON config file and print export commands."""
+    with open(json_file, 'r') as file:
+        config = json.load(file)
+
+    for key, value in config.items():
+        print(f"export {key}='{value}'")
+
+if __name__ == "__main__":
+    config_file = '/home/pinaka/config.json'  # Path to your JSON config file
+    load_config(config_file)
