803e66a7e831ec727f0a2d31461925648509c8f5
GOPI <111727423+gopipanda@users.noreply.github.com>
2024-10-22 14:04:08 +0530

    Update script8.sh

--- script8.sh
+++ script8.sh
@@ -3,7 +3,7 @@ set -x
 
 # Accessing environment variables
 source /home/pinaka/all_in_one/vpinakastra/bin/activate
-export ANSIBLE_PYTHON_INTERPRETER=/root/vpinakastra/bin/python
+export ANSIBLE_PYTHON_INTERPRETER=/home/pinaka/all_in_one/vpinakastra/bin/python
 kolla-ansible post-deploy
 pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/2023.1
 /home/pinaka/all_in_one/vpinakastra/share/kolla-ansible/init-runonce
